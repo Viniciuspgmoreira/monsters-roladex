346ed81b11cbcbc07e70fcdb942af0d90459af23
Vinicius Moreira <viniciuspgmoreira@hotmail.com>
2021-03-30 14:24:16 -0300

    Added cards and input method

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
 import React, { Component } from 'react'
 import { CardList } from '../src/components/card-list/card-list'
+import { Input } from '@material-ui/core';
+import './App.css'
 class App extends Component {
   constructor() {
     super();
 
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ''
     }
 
   }
   componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users')
       .then(response => response.json())
-      .then(users => this.setState({ monsters: users }))
+      .then(users => this.setState({ monsters: users }));
   }
 
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    )
+
     return (
       <div className="app">
-        <CardList monsters={this.state.monsters}></CardList>
+        <Input onChange={(e) => this.setState({ searchField: e.target.value })}
+          className="input-text" variant="filled" type="search" color="primary" label="Monsters..." placeholder="monters..."></Input>
+        <CardList monsters={filteredMonsters}></CardList>
 
       </div>
     )
